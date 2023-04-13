dd32485088e931c729f4b6dd62d3427254214705
Kahina Berrahrah <127066007+KBerrahrah@users.noreply.github.com>
2023-04-13 14:04:43 -0400

    Add files via upload
    Block 12: Unit 1 Career Simulation: Personal Website Pt II

--- javascript.js
+++ javascript.js
@@ -2,3 +2,23 @@
 let y = " Hello"
 let z = "Word"
 console.log(y + z)
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
